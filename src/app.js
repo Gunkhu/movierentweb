1934505d5b1b2c4073b062bfff31d5785b7ce01f
Csorba Péter <csosaka21@gmail.com>
2026-05-28 14:08:34 +0200

    Változtatások

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import {getMovies,createMovie} from './apiService.js'
 import {register,login} from './authService.js'
-
+const doc = {
+    loginButton = document.querySelector('#loginButton')
+}
+doc.loginButton.addEventListener('click',()=>{
+    console.log('Belépés indul....')
+    startLogin()
+})
+function startLogin(){
+    const user = {
+        name: 'mari',
+        password: 'titok'
+    }
+    login(user)
+}
 function startCreateMovie(){
     const movie= {
         title:'zzz',
         director:'zzz',
         releaseYear:2026
     }
+
 
     //TODO: folytatás
 }
--- src/authService.js
+++ src/authService.js
@@ -1,7 +1,22 @@
+const host ='http://localhost:8000/api/'
+
 function register(){}
 
 function login(user){
-    fetch()
+    const url = host + 'login'
+    fetch(url,{
+        method: 'post',
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    })
+    .then(response=> response.json())
+    .then(result =>{
+        console.log(result)
+    })
+    .catch(err=>console.error(err))
+    
 }
 
 
